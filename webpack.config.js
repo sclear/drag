1cbda39102e61058b86c5445c93b502185340473
jc <1392227049@qq.com>
2020-05-27 17:48:00 +0800

    更新配置

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  devtool: "source-map", 
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ // 打包输出HTML
+      title: 'Hello World app',
+      template: './dist/index.html',
+      minify: { // 压缩HTML文件
+        removeComments: true, // 移除HTML中的注释
+        collapseWhitespace: true, // 删除空白符与换行符
+        minifyCSS: true// 压缩内联css
+      },
+      filename: 'index.html',
+      //   template: 'index.html'
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "./dist"),
+    compress: true,
+    port: 9000,
+    hot: true,
+    inline: true,
+  }
+}
